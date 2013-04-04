118644088196620d00f3651e04aac3f47be1d491
ericz <eric@zinnikas.org>
2013-04-04 17:47:17 -0400

    Added CAS test file.

---
+++ cas_test.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once './CAS/config.php';
+require_once $phpcas_path . '/CAS.php';
+
+phpCAS::setDebug();
+
+phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
+
+phpCAS::setNoCasServerValidation();
+
+phpCAS::forceAuthentication();
+
+if( isset($_REQUEST['logout'])) {
+  phpCAS::logout();
+}
+
+?>
+<html>
+  <head>
+    <title>phpCAS simple client</title>
+  </head>
+  <body>
+    <h1>Successfull Authentication!</h1>
+    <?php require 'script_info.php' ?>
+    <p>the user's login is <b><?php echo phpCAS::getUser(); ?></b>.</p>
+    <p>phpCAS version is <b><?php echo phpCAS::getVersion(); ?></b>.</p>
+    <p><a href="?logout=">Logout</a></p>
+  </body>
+</html>
