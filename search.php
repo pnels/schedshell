6bfbb1bcbc1839f00d7cc0947844afd2d5dc8d07
ericz <eric@zinnikas.org>
2013-04-30 21:54:27 -0400

    Fix spacing.

--- search.php
+++ search.php
@@ -45,7 +45,7 @@
             <?php printNavlist("goals"); ?>
             <div class='offset2 span6'>
 <div class='row-fluid'>
-<div class='offset5 span5' style='overflow: hidden;' id="center-container"><div style="width:600px;height:400px;overflow: hidden;" id="treediv"></div></div>
+<div class='offset5 span5' style='overflow: hidden;' id="center-container"><div class='span5' style="overflow: hidden;" id="treediv"></div></div>
 </div>
               <!-- need to style table so it isn't so huge....only need like max 5 chars in each input box -->
               <form action='' method='POST'>
