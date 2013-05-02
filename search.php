ff26c696192b493c5656dbc0db7f4f6e39a4f8b1
Paige <pnels@terpmail.umd.edu>
2013-05-01 22:02:47 -0400

    more comments to search

--- search.php
+++ search.php
@@ -37,9 +37,9 @@
         </style>
 
 	</head>	
-
+  <!-- will actually create the tree -->
     <body <?php if( isset($_POST['goal']) && preg_match("/\w{4}\d{3}\w?/", $_POST['goal']) ) { echo 'onload="init();"'; } ?> >
-
+      <!--formatting navigation bar -->
     <?php
       printNavbar("pre");
     ?>
@@ -52,6 +52,7 @@
               <form action='' method='POST'>
               <table class='table table-bordered table-hover'>
                 <thead><tr><th style='text-align: center;'>Course Code</th></tr></thead>
+                <!-- Defines the input box, and the variable that represents it (called goal). CMSC132 will be displayed as default-->
                 <tbody><tr><td><input type='text' name='goal' placeholder='CMSC132' /></td></tr></tbody>
               </table>  
               </form>
@@ -59,6 +60,7 @@
 <div class='offset2 span8' style='overflow: hidden;' id="center-container"><div class='span10' style="height: 350px; overflow: hidden;" id="treediv"></div></div>
 </div>
 <br /><br />
+<!-- Logs into MySQL database, localhost: server location, hardshell: MySQL username, d0ntgue55m3: password, hardshell: databasename -->
 <?php
 if( isset($_POST['goal']) && preg_match("/\w{4}\d{3}\w?/", $_POST['goal']) ) {
   $mysqli = new mysqli("localhost", "hardshell", "d0ntgue55m3", "hardshell");
@@ -70,6 +72,7 @@ if( isset($_POST['goal']) && preg_match("/\w{4}\d{3}\w?/", $_POST['goal']) ) {
   $arr = array();
 
 ?>
+<!-- Creates the table that is displayed after course entered-->
 <table class='table table-bordered table-condensed table-hover'>
   <thead>
     <tr>
@@ -80,15 +83,17 @@ if( isset($_POST['goal']) && preg_match("/\w{4}\d{3}\w?/", $_POST['goal']) ) {
     </tr>
   </thead>
   <tbody>
+<!-- gets prerequisites for a given course by an already populated database -->
 <?php
-
   function getPre($ccode,$mysqli) {
   global $safe, $arr;
   $arr[] = $ccode;
+  //Some courses a prereqs to themselves (for whatever reason); this prevents infinite looping
   if( $safe >= 10 ) {
      return;
   }
   $safe++;
+  //Series of steps to prevent SQL injection while accessing database
   $stmt = $mysqli->prepare("SELECT name, credits, prereqs, coreqs, description FROM course_info WHERE name = ?");
   $param = $ccode;
   $stmt->bind_param('s', $param);
@@ -113,6 +118,7 @@ if( isset($_POST['goal']) && preg_match("/\w{4}\d{3}\w?/", $_POST['goal']) ) {
       }
       echo "</tr>";
   }
+  //Prereqs in database are stored as a single string with course names divided by commas. Explode separates that string based on the commas.
   $prqs = explode( ", ", $prereqs );
   foreach ($prqs as $course) {
     if( $course == $ccode ) {
@@ -121,6 +127,7 @@ if( isset($_POST['goal']) && preg_match("/\w{4}\d{3}\w?/", $_POST['goal']) ) {
     if( in_array( $course, $arr ) ) {
       continue;
     }
+    //Actually calling our function
     getPre($course,$mysqli);
   }
   }
@@ -131,6 +138,7 @@ if( isset($_POST['goal']) && preg_match("/\w{4}\d{3}\w?/", $_POST['goal']) ) {
 </tbody>
 </table>
 
+<!-- If user enters an invalid course, request will not be sent -->
 <?php
 } elseif( isset($_POST['goal']) ) {
   echo '<b>Please enter a valid course code.</b>'; 
