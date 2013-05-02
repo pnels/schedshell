8d3d27ef03e878ed3062e415617992dbb56e477e
ericz <eric@zinnikas.org>
2013-05-01 22:00:56 -0400

    added 2nd table

--- plan.php
+++ plan.php
@@ -37,7 +37,7 @@
         <!-- START: main page -->
         <div class='row-fluid'>
             <?php printNavlist("plan"); ?>
-            <div class='offset2 span6'>
+            <div class='offset1 span3'>
 
 <!-- apparently if you're not logged in all post variables are erased? wtf...something to do w/CAS -->
 <?php 
@@ -104,10 +104,14 @@ foreach( $list as $class ) {
 
 ?>
 <table class='table table-bordered table-hover'>
-<thead><tr><th style='text-align: center;'>Courses You Can Take</th></tr></thead>
+<thead>
+<tr><th style='text-align: center;'>Eligible</th></tr>
+</thead>
 <tbody>
 <?
 foreach( $oklist as $class ) {
+// remove from list to display @ end
+unset($list[array_search($class, $list)]);
 ?>
 <tr><td><?php echo $class; ?></td></tr>
 <?php
@@ -115,6 +119,22 @@ foreach( $oklist as $class ) {
 ?>
 </tbody>
 </table>
+</div>
+<div class='offset1 span3'>
+<table class='table table-bordered table-hover'>
+<thead>
+<tr><th style='text-align: center;'>Not Yet Eligible</th></tr>
+</thead>
+<tbody>
+<?php
+foreach( $_POST['taken'] as $class ) { unset($list[array_search($class, $list)]); }
+foreach( $list as $class ) {
+?>
+<tr><td><?php echo $class; ?></td></tr>
+<?php
+}
+?>
+</tbody>
 <? } ?>
             </div>
         </div>
